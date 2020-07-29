81e22c7e9f55e988f18978be11e6c4336c8e8086
aromsa <annacbateman@gmail.com>
2020-07-29 18:48:00 -0400

    cart delete complete

--- app/controllers/api/v1/cart_items_controller.rb
+++ app/controllers/api/v1/cart_items_controller.rb
@@ -10,6 +10,16 @@ class Api::V1::CartItemsController < ApplicationController
         render json: cart_item, include: :item, status: :ok
     end
 
+    def destroy 
+        cart_item = CartItem.find_by(id: params[:id])
+        cart_item.destroy
+    end 
+    
+    def show
+        cart_item = CartItem.find_by(id: params[:id])
+        render json: cart_item
+    end
+
     private
     def cart_item_params
         params.require(:cart_item).permit(:cart_id, :item_id)
