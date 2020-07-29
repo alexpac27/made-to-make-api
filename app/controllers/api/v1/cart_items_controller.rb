0ea14a2f251937a440c5d693466c2e2764e56628
aaltmark <adamaltmark@gmail.com>
2020-07-29 17:31:36 -0400

    add to cart

--- app/controllers/api/v1/cart_items_controller.rb
+++ app/controllers/api/v1/cart_items_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::CartItemsController < ApplicationController
+
+    def index
+        cart_items = CartItem.all 
+        render json: cart_items, include: :item, status: :ok
+    end
+
+    def create
+        cart_item = CartItem.create!(cart_item_params)
+        render json: cart_item, include: :item, status: :ok
+    end
+
+    private
+    def cart_item_params
+        params.require(:cart_item).permit(:cart_id, :item_id)
+    end
 end
